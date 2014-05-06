09857e8952b3826b6e9429aa778edaf8c070ace4
helgadb <helgadb@midiacom.uff.br>
2014-05-06 12:01:03 -0300

    Update SCIFICore.sh

--- install/modules/SCIFICore.sh
+++ install/modules/SCIFICore.sh
@@ -25,6 +25,7 @@ This module will:
 a) Install SCIFI Core, rev 206 (svn) 
 b) Create linux user "scifi" and set his password 
 c) Create SSH key for communication between the APs and scifi
+d) Initialize SCIFI core and web
 
 Press <Enter> key
 
@@ -57,12 +58,12 @@ if [ $java -eq 0 ]
     # resetting configurations
     if [ -f /usr/share/scifi/core/controller_key ]; then cp /usr/share/scifi/core/controller_key $ModDir/SCIFICore/controller_key.old.$(date +%Y%m%d-%H%M%S);fi; 
     if [ -f /usr/share/scifi/core/authorized_keys ]; then cp /usr/share/scifi/core/authorized_keys $ModDir/SCIFICore/authorized_keys.old.$(date +%Y%m%d-%H%M%S);fi; 
-    rm -rf /usr/share/scifi/core
+    rm -rf /usr/share/scifi/core 2> /dev/null
     sed -i '/sh \/usr\/share\/scifi\/core\/StartController.sh/d' /etc/rc.local 
     # a) Install SCIFI core
     mkdir /usr/share/scifi/core
     cp -r $ModDir/SCIFICore/* /usr/share/scifi/core
-    rm /usr/share/scifi/core/*old*
+    rm /usr/share/scifi/core/*old* 2> /dev/null
     echo $SCIFIDBPASSWD >> /usr/share/scifi/core/login_config
     echo "sh /usr/share/scifi/core/StartController.sh" >> /etc/rc.local
     
@@ -77,6 +78,9 @@ if [ $java -eq 0 ]
     mv /usr/share/scifi/core/controller_key.pub /usr/share/scifi/core/authorized_keys
     chmod 0600 /usr/share/scifi/core/controller_key
     chmod 0600 /usr/share/scifi/core/authorized_keys
+
+    #d) Initialize SCIFI core and web
+    sh /usr/share/scifi/core/StartController.sh
    
   fi
  fi
