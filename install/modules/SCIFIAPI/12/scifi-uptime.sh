451a46cf29dde071eb452a85ea0cd7eeee8baad1
glaucoq <glaucoq@id.uff.br>
2014-06-03 10:02:36 -0300

    Update scifi-uptime.sh

--- install/modules/SCIFIAPI/12/scifi-uptime.sh
+++ install/modules/SCIFIAPI/12/scifi-uptime.sh
@@ -1,4 +1,4 @@
-!/bin/sh
+#!/bin/sh
 # version 20140527
 # Return the type of device
 # Cosme Corr..a
@@ -7,7 +7,14 @@
 #set -xv
 
 
-RESULT=$(cat /proc/uptime |awk '{print $1}')
-
-echo $RESULT
+UPTIMEOID=.1.3.6.1.2.1.25.1.1.0
+#UPTIMEOID=.1.3.6.1.6.3.10.2.1.3
+DEVICE=127.0.0.1
+VERSION=2c
+COMMUNITY=public
+#RESULT=`snmpget -v $VERSION -c $COMMUNITY $DEVICE $UPTIMEOID | cut -d'(' -f2 | cut -d')' -f1 `
+RESULT=$(cat /proc/uptime |awk '{print $1}' | cut -d. -f1 )
+#echo $RESULT
+MINUTES=$((RESULT/60))
+echo $MINUTES
 exit 0
