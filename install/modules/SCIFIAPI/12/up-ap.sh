03ae50c4c498d24fc57f337978ca2cbc2acaaf62
glaucoq <glaucoq@id.uff.br>
2014-06-17 15:21:40 -0300

    Update up-ap.sh

--- install/modules/SCIFIAPI/12/up-ap.sh
+++ install/modules/SCIFIAPI/12/up-ap.sh
@@ -36,32 +36,38 @@ if [ "`/usr/share/scifi/scripts/scifi-type.sh`" = "CONTROLLER" ]
         echo 'O AP já esta atualizado'
         else
 
-#       export PATH=/bin:/sbin:/usr/bin:/usr/sbin;
+AP
+        if [ "`/usr/share/scifi/scripts/scifi-version.sh >/dev/null; echo $?`" = "12" ]
+        then
+        echo 'O AP já esta atualizado'
+        else
+
         LOCATION=$(cat /etc/config/snmpd |grep sysLocation)
         NAME=$(cat /etc/config/snmpd |grep sysName)
         mkdir /etc/scifi
         mkdir /usr/share/scifi
         mkdir /usr/share/scifi/scripts
-        mv /etc/dropbear/authorized_keys /etc/scifi
-        ln -s /etc/scifi/authorized_keys /etc/dropbear/
+        ln -s /etc/dropbear/authorized_keys /etc/scifi
         sh /etc/scripts/ap_type.sh > /etc/scifi/scifi-type.txt
         > /etc/scifi/scifi-connected2.txt
         > /etc/scifi/scifi-coordinates.txt
         > /etc/scifi/scifi-tags.txt
         > /etc/scifi/scifi-neighborhood.txt
         mv /etc/scripts/* /usr/share/scifi/scripts 2>/dev/null
-        ln -s /usr/share/scifi/scripts/* /etc/scripts
 #       copy this
         cp -f /tmp/SCIFIAPI/*.sh /usr/share/scifi/scripts/
+        rm -f /usr/share/scifi/scripts/up-ap.sh
+        ln -s /usr/share/scifi/scripts/* /etc/scripts
         cp -f /tmp/SCIFIAPI/scifi-version.txt /etc/scifi/
         cp -f /tmp/SCIFIAPI/scifi-subversion.txt /etc/scifi/
+#       SNMP
         cp -f /tmp/SCIFIAPI/snmpd /etc/config/snmpd
         sed -i "76s/^.*/$LOCATION /" /etc/config/snmpd
         sed -i "78s/^.*/$NAME /" /etc/config/snmpd
-        chmod 700 /usr/share/scifi/scripts/ -R
         rm -f /etc/snmp/snmpd.conf
         ln -s /var/run/snmpd.conf /etc/snmp/
         /etc/init.d/snmpd restart
+        chmod 700 /usr/share/scifi/scripts/ -R
 fi
 fi
 exit 0
