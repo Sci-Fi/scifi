f23cdb8cc9a70348d974d58eeba85b86dd786074
Cosme Faria Corrêa <cosmefc@gmail.com>
2014-06-18 14:09:44 -0400

    up-ap.sh fix

--- install/modules/SCIFIAPI/12/up-ap.sh
+++ install/modules/SCIFIAPI/12/up-ap.sh
@@ -1,4 +1,5 @@
 # Update API in APs
+# Version 20140618
 # Cosme Corrêa - cosmefc@id.uff.br
 # Glauco Quintino glaucoq@id.uff.br
 # uncomment for debug
@@ -8,9 +9,9 @@ ERRO () {
 echo 
 echo 'Update APs API'
 echo 
-echo sintax:   $0  
+echo sintax:   $0 DEVICE
 echo 
-echo example:   $0 
+echo example:   $0 ap0050
 echo 
 exit
 }
@@ -25,49 +26,51 @@ fi
 
 if [ "`/usr/share/scifi/scripts/scifi-type.sh`" = "CONTROLLER" ]
         then
-#       copy pacakge to ap
-        scp -pri /etc/scifi/controller_key /etc/scifi/SCIFIAPI root@$1:/tmp/
-#       execute it remotely
-        ssh -i /etc/scifi/controller_key  root@$1 '/tmp/SCIFIAPI/up-ap.sh 1'
+#       	copy package to ap
+        	scp -pri /etc/scifi/controller_key /etc/scifi/SCIFIAPI root@$1:/tmp/
+#       	execute it remotely
+        	ssh -i /etc/scifi/controller_key  root@$1 '/tmp/SCIFIAPI/up-ap.sh 1'
         else
-#       AP
-        if [ "`/usr/share/scifi/scripts/scifi-version.sh >/dev/null; echo $?`" = "12" ]
-        then
-        echo 'O AP já esta atualizado'
-        else
+#       	AP
+        	if [ "`/usr/share/scifi/scripts/scifi-version.sh >/dev/null; echo $?`" = "12" ]
+        	then
+        		echo 'O AP %0 já esta atualizado'
+	        else
 
-AP
-        if [ "`/usr/share/scifi/scripts/scifi-version.sh >/dev/null; echo $?`" = "12" ]
-        then
-        echo 'O AP já esta atualizado'
-        else
+			LOCATION=$(cat /etc/config/snmpd |grep sysLocation) 
+			NAME=$(cat /etc/config/snmpd |grep sysName) 
+        		mkdir /etc/scifi
+        		mkdir /usr/share/scifi
+        		mkdir /usr/share/scifi/scripts
+        		ln -s /etc/dropbear/authorized_keys /etc/scifi
+        		sh /etc/scripts/ap_type.sh > /etc/scifi/scifi-type.txt
+        		touch /etc/scifi/scifi-connected2.txt
+        		touch /etc/scifi/scifi-coordinates.txt
+        		touch /etc/scifi/scifi-tags.txt
+        		ln -s /etc/scifi/scifi-neighborhood.txt /tmp/scifi-neighborhood.txt
+			rm -f /etc/scripts/nsta.sh
+			rm -f /etc/scripts/ap_type.sh
+			rm -f /etc/scripts/SCIFI.sh
+        		mv /etc/scripts/* /usr/share/scifi/scripts 2>/dev/null
+#       		copy this
+        		cp -f /tmp/SCIFIAPI/*.sh /usr/share/scifi/scripts/
+        		rm -f /usr/share/scifi/scripts/up-ap.sh
+			ln -s /usr/share/scifi/scripts/* /etc/scripts
+#			For retrocompatibility, these will be removed in the future
+			ln -s /usr/share/scifi/scripts/scifi-users.sh /etc/scripts/nsta.sh
+			ln -s /usr/share/scifi/scripts/scifi-type.sh /etc/scripts/ap_type.sh
 
-        LOCATION=$(cat /etc/config/snmpd |grep sysLocation)
-        NAME=$(cat /etc/config/snmpd |grep sysName)
-        mkdir /etc/scifi
-        mkdir /usr/share/scifi
-        mkdir /usr/share/scifi/scripts
-        ln -s /etc/dropbear/authorized_keys /etc/scifi
-        sh /etc/scripts/ap_type.sh > /etc/scifi/scifi-type.txt
-        > /etc/scifi/scifi-connected2.txt
-        > /etc/scifi/scifi-coordinates.txt
-        > /etc/scifi/scifi-tags.txt
-        > /etc/scifi/scifi-neighborhood.txt
-        mv /etc/scripts/* /usr/share/scifi/scripts 2>/dev/null
-#       copy this
-        cp -f /tmp/SCIFIAPI/*.sh /usr/share/scifi/scripts/
-        rm -f /usr/share/scifi/scripts/up-ap.sh
-        ln -s /usr/share/scifi/scripts/* /etc/scripts
-        cp -f /tmp/SCIFIAPI/scifi-version.txt /etc/scifi/
-        cp -f /tmp/SCIFIAPI/scifi-subversion.txt /etc/scifi/
-#       SNMP
-        cp -f /tmp/SCIFIAPI/snmpd /etc/config/snmpd
-        sed -i "76s/^.*/$LOCATION /" /etc/config/snmpd
-        sed -i "78s/^.*/$NAME /" /etc/config/snmpd
-        rm -f /etc/snmp/snmpd.conf
-        ln -s /var/run/snmpd.conf /etc/snmp/
-        /etc/init.d/snmpd restart
-        chmod 700 /usr/share/scifi/scripts/ -R
-fi
+        		cp -f /tmp/SCIFIAPI/scifi-version.txt /etc/scifi/
+        		cp -f /tmp/SCIFIAPI/scifi-subversion.txt /etc/scifi/
+#       		SNMP
+        		cp -f /tmp/SCIFIAPI/snmpd.ap /etc/config/snmpd
+        		sed -i "76s/^.*/$LOCATION /" /etc/config/snmpd
+			sed -i "78s/^.*/$NAME /" /etc/config/snmpd
+        		rm -f /etc/snmp/snmpd.conf
+        		ln -s /var/run/snmpd.conf /etc/snmp/
+			/etc/init.d/snmpd restart
+			chmod 700 /usr/share/scifi/scripts/ -R
+	
+		fi
 fi
 exit 0
