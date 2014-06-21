6c0a73a336a3ce20c3053441d730dfd35fcaf1a0
Cosme Faria Corrêa <cosmefc@gmail.com>
2014-06-20 20:59:35 -0400

    New design for API 12
    fix uptime from seconds to minutes
    new delimitator for scifi.cfg

--- install/modules/SCIFIAPI/12/scifi-api.sh
+++ install/modules/SCIFIAPI/12/scifi-api.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 # version 20140620
+# Central script for SCIFI-API
+#
 # Luiz Magalhaes
 # schara (at) telecom.uff.br
-#
+# Cosme Corr..a
+# cosmefc@id.uff.br
 # uncomment for debug
 #set -xv
 
 case "$1" in
 
 ID | VERSION | SUBVERSION | DEVICE | COORDINATES | TAGS | CONNECTED2)
-awk -v search=$1 '{if ($1==search) print $2;}' /tmp/scifi/scifi-snmp.conf
+awk -F'=' -v search=$1 '{if ($1==search) print $2;}' /etc/scifi/scifi.conf
         ;;
 
 USERS) iw wlan0 station dump | grep -c Station
@@ -18,12 +21,12 @@ USERS) iw wlan0 station dump | grep -c Station
 NEIGHBORHOOD) if [ -f "/tmp/scifi-neighborhood.txt"];
                 then
                 cat /tmp/scifi-neighborhood.txt
-                else
-                echo "0"
+#                else
+#                echo "0"
                 fi
         ;;
 
-UPTIME) echo `cut -d' ' -f1 /proc/uptime | cut -d'.' -f1`
+UPTIME) echo $((`cut -d'.' -f1 /proc/uptime`/60))
 
         ;;
 
