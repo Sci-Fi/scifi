bc5cd1f32a339a97e0507f28fc1b81bdad2f8271
Cosme Faria Corrêa <cosmefc@gmail.com>
2014-06-23 11:56:21 -0400

    Fix call #8

--- install/modules/SCIFIAPI/12/scifi-api.sh
+++ install/modules/SCIFIAPI/12/scifi-api.sh
@@ -17,7 +17,25 @@ awk -F'=' -v search=$1 '{if ($1==search) print $2;}' /etc/scifi/scifi.conf
         ;;
 
 # Call #8
-USERS) iw wlan0 station dump | grep -c Station
+USERS) 
+if [ "`snmpget -v 2c -c public 127.0.0.1 .1.3.6.1.4.1.2021.8.1.101.4 | cut -d' ' -f4`" = "CONTROLLER" ]
+	then
+	LIST=`cut -d'/' -f5 /etc/mrtg/devices.inc | grep ap`
+	for AP in $LIST
+		do
+		AP=`expr substr $AP 1 6`
+		USU=0		
+		if [ `head -1 /var/www/mrtg/"$AP"/"$AP"_usu.log | cut -d' ' -f3` != '-1' ] 
+			then
+			echo USU=`head -2 /var/www/mrtg/"$AP"/"$AP"_usu.log | tail -1 | cut -d' ' -f2`
+		fi
+		USU=${USU/' '/}
+		TOTAL=$(( TOTAL + USU ))
+		done
+	echo $TOTAL
+	else
+	echo $((`iw wlan0 station dump|grep -c Station`+`iw wlan0-1 station dump | grep -c Station`))
+fi
         ;;
 
 # Call #9
