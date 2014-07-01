46a5848d059175cbde0892572b0349b5533b54a3
luizschara <luizschara@gmail.com>
2014-07-01 00:31:18 -0300

    Update vlan-check_SC.sh

--- install/modules/SCIFIAPI/12/vlan-check_SC.sh
+++ install/modules/SCIFIAPI/12/vlan-check_SC.sh
@@ -12,15 +12,53 @@
 
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin;
 
-
 function turn_on_wifi {
 
 # Parameters:
 # $1 -> interface : wlan0, wlan0-1, wlan0-2
 # 
+case $1 of
 
+wlan0)
+uci set wireless.@wifi-iface[0].disabled=0
+;;
+"wlan0-1")
+uci set wireless.@wifi-iface[1].disabled=0
+;;
+"wlan0-2")
+uci set wireless.@wifi-iface[2].disabled=0
+;;
+esac
+uci commit wireless
+wifi
 
 	}
+
+
+function turn_off_wifi {
+
+# Parameters:
+# $1 -> interface : wlan0, wlan0-1, wlan0-2
+# 
+case $1 of
+
+wlan0)
+uci set wireless.@wifi-iface[0].disabled=1
+;;
+"wlan0-1")
+uci set wireless.@wifi-iface[1].disabled=1
+;;
+"wlan0-2")
+uci set wireless.@wifi-iface[2].disabled=1
+;;
+esac
+
+uci commit wireless
+wifi
+
+	}
+
+
 
 
 
