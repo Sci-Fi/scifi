220100bb89a5b13987a88ab820b85ed9e9f7bbbb
helgadb <helgadb@midiacom.uff.br>
2014-07-02 13:36:32 -0300

    corrigindo script vlan-check_PV.sh

--- install/modules/SCIFIAPI/12/vlan-check_PV.sh
+++ install/modules/SCIFIAPI/12/vlan-check_PV.sh
@@ -8,28 +8,27 @@
 # Glaudco
 # 
 # uncomment for debug
-#set -xv
+set -xv
 
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin;
 
 # random number to randomize wait time to prevent address collision and to generate temporary addresses
 
 random=`head /dev/urandom | tr -dc "0123456789" | head -c2`
-sleep $random 
 
+# tirei para testes
+# sleep $random 
 
 ifconfig br-203 192.168.0.1$random netmask 255.255.128.0
 ifconfig br-204 192.168.128.1$random netmask 255.255.128.0
+ifconfig br-205 10.2.0.1$random netmask 255.255.0.0
 ping203=$(ping -I br-203 -w10 192.168.0.1 |  grep loss | awk '{print $4;exit}')
 ping204=$(ping -I br-204 -w10 192.168.128.1 |  grep loss | awk '{print $4;exit}')
-pinglan=$(ping -I br-204 -w10 172.17.0.1 |  grep loss | awk '{print $4;exit}')
-ping205=$(ping -I br-lan -w10 10.2.0.1 | grep loss | awk '{print $4;exit}')
-ifconfig br-204 0.0.0.0
+pinglan=$(ping -I br-lan -w10 172.17.0.1 |  grep loss | awk '{print $4;exit}')
+ping205=$(ping -I br-205 -w10 10.2.0.1 | grep loss | awk '{print $4;exit}')
 ifconfig br-203 0.0.0.0
-
-WIFI0= "wlan0"
-WIFI1= "wlan0-1"
-WIFI2= "wlan0-2"
+ifconfig br-204 0.0.0.0
+ifconfig br-205 0.0.0.0
 
 # verificando comunicacao na br-205 (em bridge com wlan0)
 
@@ -49,67 +48,80 @@ WIFI2= "wlan0-2"
 # $1 -> interface : wlan0, wlan0-1, wlan0-2
 # $2 -> 0/1, 0 if not pinging server through vlan, 1 if pinging
 
-for loopcount in 1 2 3; do
-	case loopcount in
+for loopcount in "1" "2" "3"; do
+	echo "loopcount " $loopcount
 	
-	1) interface= "wlan0"
-	   pngst= $ping205
-	   ;;
-	2) interface= "wlan0-1"
-	   pngst= $ping203
-	   ;;
-	3) interface= "wlan0-2"
-	   pngst= $ping203
-	   ;;
-    	esac
-	if  [$(! /sbin/ifconfig $interface |/bin/grep UP| /usr/bin/wc -l  )="0"];
+	case "$loopcount" in
+	
+		"1") echo "1"
+		   interface="wlan0"
+		   echo "interface" $interface
+		   pngst=$ping205
+	   	   ;;
+   	   	"2") echo "2" 
+   	   	   interface="wlan0-1"
+   		   pngst=$ping203
+	   	   ;;
+   	   	"3") echo "3"
+   	   	   interface="wlan0-2"
+   		   pngst=$ping203
+	   	   ;;
+   	       	esac
+   	echo " interface " $interface " pngst" $pngst
+	if  [ $(! /sbin/ifconfig $interface |/bin/grep UP| /usr/bin/wc -l  ) = "0" ];
 	  then 
 # desligado
-  	  if  [$pngst = "1"]; 
-  		then
+echo "ping status" $pngst
+    	  if  [ $pngst = "1" ]; 
+		then
 # pinging, turn on interface, zero status
 # ligar interface, colocar zero no status
 #		turn_on_wifi $1
-		case $interface in
-		wlan0)
-		uci set wireless.@wifi-iface[0].disabled=0
-		;;
-		"wlan0-1")
-		uci set wireless.@wifi-iface[1].disabled=0
-		;;
-		"wlan0-2")
-		uci set wireless.@wifi-iface[2].disabled=0
-		;;
+echo $interface
+		case "$interface" in
+			"wlan0")
+			uci set wireless.@wifi-iface[0].disabled=0
+			;;
+			"wlan0-1")
+			uci set wireless.@wifi-iface[1].disabled=0
+			;;
+			"wlan0-2")
+			uci set wireless.@wifi-iface[2].disabled=0
+			;;
 		esac
 		uci commit wireless
 		wifi
      		logger SCIFI - Communication with server is ok. Turning $interface on.
 		echo "0"> /tmp/status$interface
 		fi
-  		else
+	else
+	echo "ping status" $pngst
 # not pinging
-		Case `cat /tmp/status$interface` in
-# está ligado, está respondendo a ping?
+	case `cat /tmp/status$interface` in
+#  está ligado, está respondendo a ping?
 
-			0) if [$pngst = "0"];
-				then
- 				echo "1"> /tmp/status$interface
-				logger SCIFI - The AP can not communicate with server. Warning 1 $interface
-				fi
-				;;
+		0) echo "status 0"
+		   if [ $pngst = "0" ];
+			then
+			echo "1"> /tmp/status$interface
+			logger SCIFI - The AP can not communicate with server. Warning 1 $interface
+			fi
+		;;
 
-			1) if [ $pngst = "0" ];
- 				then 
- 				echo "2"> /tmp/status$interface
+		1) echo "status 1"
+		   if [ $pngst = "0" ];
+			then 
+				echo "2"> /tmp/status$interface
 				logger SCIFI - The AP can not communicate with server. Warning 2 $interface
-				else 
+			else 
 				echo "0"> /tmp/status$interface
 				logger SCIFI - Communication with the server is ok. $interface
-				fi
-				;;
-
-			2) if [ $pngst = "0" ];
-				then
+			fi
+		;;
+			   	   		   	   		   	       			    	    																												     						  											 																			 				 																												
+		2) echo "status 2"
+		   if [ $pngst = "0" ];
+			then
 				logger SCIFI - The AP can not communicate with server. Turning off $interface
 # turn_off_wifi $1
 				case $interface in
@@ -122,16 +134,16 @@ for loopcount in 1 2 3; do
 				esac
 				uci commit wireless
 				wifi
-				else 
+			else 
 				echo "0"> /tmp/status$interface
 				logger SCIFI - Communication with server is ok. $interface
-				fi
-				;;
-			esac
 			fi
-  		fi
-	fi
+		;;
+		*) echo "0"> /tmp/status$interface
+		;;
+		esac
+		fi
+#	fi
 # }
 done
-
 exit 0
