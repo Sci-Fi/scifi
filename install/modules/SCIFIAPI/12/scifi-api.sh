95aaf455513eb66ca8964974d9d8fbfbc76cbdd8
helgadb <helgadb@midiacom.uff.br>
2014-07-09 15:34:41 -0300

    testing and debugging API 12. Inserting new OID called FIRMWARE

--- install/modules/SCIFIAPI/12/scifi-api.sh
+++ install/modules/SCIFIAPI/12/scifi-api.sh
@@ -11,20 +11,19 @@
 
 case "$1" in
 
-# Calls # 1,2,3,4,5,6 , 7 and 11
-ID | VERSION | SUBVERSION | DEVICE | COORDINATES | TAGS | CONNECTED2 | SEGMENT)
-awk -F'=' -v search=$1 '{if ($1==search) print $2;}' /etc/scifi/scifi.conf
+# Calls # 1,2,3,4,5,6 , 7, 11 and 12
+ID | VERSION | SUBVERSION | DEVICE | COORDINATES | TAGS | CONNECTED2 | SEGMENT | FIRMWARE)
+awk -v search=$1 '{if ($1==search) print $2;}' /etc/scifi/scifi.conf
         ;;
 
 # Call #8
 USERS) 
 # this version for APs only
-	echo $((`iw wlan0 station dump|grep -c Station`+`iw wlan0-1 station dump | grep -c Station`))
-
+	nsta=0;for i in `ifconfig | grep wlan0 | awk '{print $1}'`; do let nsta=$nsta+`iw $i station dump | grep -c Station`;done;echo $nsta;	
         ;;
 
 # Call #9
-NEIGHBORHOOD) if [ -f "/tmp/scifi-neighborhood.txt"];
+NEIGHBORHOOD) if [ -f "/tmp/scifi-neighborhood.txt" ];
                 then
                  cat /tmp/scifi-neighborhood.txt
                 else
