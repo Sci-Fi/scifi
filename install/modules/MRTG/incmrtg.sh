77c6914b4b0a2c54de86e7b7d68b520380780a1a
luizschara <schara@luizs-air.home>
2014-07-25 01:24:38 -0300

    incmrtg.sh
    includes or refreshes settings for each AP on MRTG

--- install/modules/MRTG/incmrtg.sh
+++ install/modules/MRTG/incmrtg.sh
@@ -1,27 +1,25 @@
 #!/bin/bash                           
-# version 20140627                     
+# version 20140716                     
 # Include devices for MRTG
 # Cosme Corrêa
 # cosmefc@id.uff.br
 # modified by schara@midiacom.uff.br
 # 
-# Schara 27/06/2014
-# This implementation deals with two versions of the SCIFI MIB
+# Schara 16/07/2014
+# This implementation deals with three versions of the SCIFI MIB
 #
-# one is 11.5, which uses OID 1.3.6.1.4.1.2021.8.1.101.2 (or 100.2) for # of users
+# one is 11 (.1,.2,.3,.4,.5), which uses OID 1.3.6.1.4.1.2021.8.1.101.2 (or 100.
+2) for # of users
 #
-# the other is 12, which uses OID .1.3.6.1.4.1.2021.8.1.101.8 for # of users.
+# another is 12, which uses OID .1.3.6.1.4.1.2021.8.1.101.8 for # of users.
+#
+# and added last is 10, which uses OID 1.3.6.1.4.1.2021.8.1.101.1 
 #
 # monitoring of wlan is turned off
 #
 
-
-
-
-
-
-# Uncomment for debug
-# set -xv
+# Uncomment for debugging
+#set -xv
 
 ERRO () {
 echo $1
@@ -58,8 +56,11 @@ ARQINC=$DirMRTG"devices.inc"
 
 case $# in
 	1)
-		#TEMPLATE=`snmpget -v 2c -c $COMMUNITY $DISPOSITIVO .1.3.6.1.4.1.2021.8.1.101.3` || ( echo "Someting is wrong about $DISPOSITIVO, test it" ; ERRO ; exit )
-		TEMPLATE=`snmpget -v 2c -c $COMMUNITY $DISPOSITIVO .1.3.6.1.4.1.2021.8.1.101.3`
+		#TEMPLATE=`snmpget -v 2c -c $COMMUNITY $DISPOSITIVO .1.3.6.1.4.1
+.2021.8.1.101.3` || ( echo "Someting is wrong about $DISPOSITIVO, test it" ; ERR
+O ; exit )
+		TEMPLATE=`snmpget -v 2c -c $COMMUNITY $DISPOSITIVO .1.3.6.1.4.1.
+2021.8.1.101.4`
 		if [ "$TEMPLATE" == "" ]
 			then
 			echo "Someting is wrong about $DISPOSITIVO, test it" ;
@@ -84,14 +85,54 @@ esac
 
 # this section is commented pending implementation of version
 # schara 07/03/2014
-## Is this a SCIFI devive?
+## Is this a SCIFI device?
 #
-#[ $SCIFIlabel != `snmpget -v 2c -c $COMMUNITY $DISPOSITIVO $SCIFIlabelOID | cut -d" " -f4-` ] && ERRO "This is not a SCIFI device"
+#[ $SCIFIlabel != `snmpget -v 2c -c $COMMUNITY $DISPOSITIVO $SCIFIlabelOID | cut
+ -d" " -f4-` ] && ERRO "This is not a SCIFI device"
 #
 ## Is this a right version od SCIFI?
-#[ $SCIFIver != `snmpget -v 2c -c $COMMUNITY $DISPOSITIVO $SCIFIverOID | cut -d" " -f4-` ] && ERRO 'Wrong version of SCIFI device' 
+#[ $SCIFIver != `snmpget -v 2c -c $COMMUNITY $DISPOSITIVO $SCIFIverOID | cut -d"
+ " -f4-` ] && ERRO 'Wrong version of SCIFI device' 
 #
 # end of commented section schara 07/03/2014
+
+
+
+# Checking version
+# schara 27/06/2014
+#
+# currently this sets the userfield (last digit) of the OID
+# and the template in case of version 11, as 12 is now the default (23/07/2014)
+
+scifiversion=`snmpget -v 2c -c $COMMUNITY $DISPOSITIVO $SCIFIlabelOID | cut -d" 
+" -f4-`
+
+echo "scifiversion " $scifiversion
+
+case $scifiversion in
+"SCIFI.11.1"|"SCIFI.11.2"|"SCIFI.11.3"|"SCIFI.11.4"|"SCIFI.11.5") 
+	echo $scifiversion
+        userfield="2"
+	TEMPLATE=`snmpget -v 2c -c $COMMUNITY $DISPOSITIVO .1.3.6.1.4.1.2021.8.1
+.101.4 | cut -d" " -f4-`
+	echo $TEMPLATE
+        ;;
+SCIFI) echo $scifiversion
+        userfield="8"
+        ;;
+
+# abaixo foi adicionado para funcionar na versão 10. Schara 16/07/2014
+
+*) echo "Versao desconhecida: " $scifiversion
+   echo "Assumindo dispositivo TL-WR743ND e versão 10 da API."
+   echo "em caso contrario pressione control-C. Se não <enter> para continuar"
+   read
+   TEMPLATE="TL-WR743ND"
+   userfield="1"
+	
+;;
+esac
+
 # Testa se MODEDLO existe em DirTemplates
 if ! [ -a $DirTemplates$TEMPLATE ]
 	then
@@ -99,32 +140,6 @@ if ! [ -a $DirTemplates$TEMPLATE ]
 	ERRO;
 	exit;
 fi
-
-
-# Checking version
-# schara 27/06/2014
-#
-# currently this sets the userfield (last digit) of the OID
-
-scifiversion=`snmpget -v 2c -c $COMMUNITY $DISPOSITIVO $SCIFIlabelOID | cut -d" " -f4-`
-
-echo "scifiversion " $scifiversion
-
-case $scifiversion in
-SCIFI.11.1) echo $scifiversion
-	userfield="2"
-	;;
-SCIFI.11.5) echo $scifiversion
-	userfield="2"
-	;;
-SCIFI) echo $scifiversion
-	userfield="8"
-	;;
-*) echo "Versao desconhecida: " $scifiversion
-;;
-esac
-
-
 
 
 # Get name using SNMP
@@ -135,18 +150,21 @@ PNOME=`echo $NOME | cut -d"." -f1`
 # Pega local por SNMP
 LOCAL=`snmpget -v 1 -c public $DISPOSITIVO sysLocation.0 | cut -d" " -f4-`
 # adicionei a linha abaixo - sch 27/07/2013
-LOCALTXT=`snmpget -v 1 -c public $DISPOSITIVO sysLocation.0 | cut -d" " -f4- | awk -F "_-22." '{print $1}'`
+LOCALTXT=`snmpget -v 1 -c public $DISPOSITIVO sysLocation.0 | cut -d" " -f4- | a
+wk -F "_-22." '{print $1}'`
 
 #retirando wlan... sch 27/06/2014
 #echo 1
 ## pega o indice da interface wlan0
-#lista=`snmpwalk -v 2c -c public $DISPOSITIVO 1.3.6.1.2.1.2.2.1.2 | grep wlan0 | awk -F "." '{print $2}'| awk '{print $1}'`
+#lista=`snmpwalk -v 2c -c public $DISPOSITIVO 1.3.6.1.2.1.2.2.1.2 | grep wlan0 |
+ awk -F "." '{print $2}'| awk '{print $1}'`
 #
 #echo 2
 #
 #for b in $lista ;
 #do
-#         if [ `/usr/bin/snmpget -v 1 -c public $DISPOSITIVO 1.3.6.1.2.1.2.2.1.7.$b |grep -c up`!=0 ]; then
+#         if [ `/usr/bin/snmpget -v 1 -c public $DISPOSITIVO 1.3.6.1.2.1.2.2.1.7
+.$b |grep -c up`!=0 ]; then
 #                oidwlan=$b
 #        fi 
 #done
