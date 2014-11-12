e3cc43fc85b3507beb9ef9b15d6e19e89398aa77
luizschara <luizschara@gmail.com>
2014-11-12 17:04:18 -0200

    adding period & group selection to MRTG user graph

--- install/modules/MRTG/varwwwmrtg/classes/user.php
+++ install/modules/MRTG/varwwwmrtg/classes/user.php
@@ -1,10 +1,27 @@
 <h3>Gr&aacute;ficos de N&Uacute;MERO DE USU&Aacute;RIOS dos dispositivos</h3>
 
+<form name="input" action="index.php" method="get">
+<input type="radio" name="periodo" value="day" checked> Di&aacuterio
+<input type="radio" name="periodo" value="week"> Semanal
+<input type="radio" name="periodo" value="month"> Mensal
+<input type="radio" name="periodo" value="year"> Anual  <br>
+<select name="conjunto">
+<option value="">Todos</option>
+<?php include_once "classes/menu_loc.html";
+?>
+</select>
+<input type="radio" name="page" value="classes" checked > Exibir
+
+<input type="radio" name="classe" value="user" checked> Usu&aacuterios
+
+<input type="submit" value="Mostrar">
+</form>
+
 <ul id="listMRTG">
 <?php
     $dir = "./";
     $type_class = "usu";
-    $time_graph_class = "day";
+    $time_graph_class = periodo;
     $list = array();
     $sort_root = array();
     $path_file = "";
