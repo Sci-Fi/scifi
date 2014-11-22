0ee386269def5479f8ebc5c1606041c3ae73b050
luizschara <luizschara@gmail.com>
2014-11-22 16:23:43 -0200

    Update vlan-check_PV.sh
    Correcting indentation

--- install/modules/SCIFIAPI/13/vlan-check_PV.sh
+++ install/modules/SCIFIAPI/13/vlan-check_PV.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# version 20140701
+# version 20141122
 # This files switches off wifi interfaces if there is no wired (vlan) access
 #
 # Luiz Magalhaes
@@ -19,38 +19,39 @@ sleep $random
 
 pinglan=$(ping -I br-lan -w10 172.17.0.1 |  grep loss | awk '{print $4;exit}')
 
-if  [ $pinglan -gt 1 ]; then
+if  [ $pinglan -gt 1 ]; 
+	then
 
-ifconfig br-203 192.168.0.1$random netmask 255.255.128.0
-ping203=$(ping -I br-203 -w10 192.168.0.1 |  grep loss | awk '{print $4;exit}')
-ifconfig br-203 0.0.0.0
+	ifconfig br-203 192.168.0.1$random netmask 255.255.128.0
+	ping203=$(ping -I br-203 -w10 192.168.0.1 |  grep loss | awk '{print $4;exit}')
+	ifconfig br-203 0.0.0.0
 
-ifconfig br-204 192.168.128.1$random netmask 255.255.128.0
-ping204=$(ping -I br-204 -w10 192.168.128.1 |  grep loss | awk '{print $4;exit}')
-ifconfig br-204 0.0.0.0
+	ifconfig br-204 192.168.128.1$random netmask 255.255.128.0
+	ping204=$(ping -I br-204 -w10 192.168.128.1 |  grep loss | awk '{print $4;exit}')
+	ifconfig br-204 0.0.0.0
 
-ifconfig br-205 10.1.0.1$random netmask 255.255.0.0
-ping205=$(ping -I br-205 -w10 10.1.0.1 | grep loss | awk '{print $4;exit}')
-ifconfig br-205 0.0.0.0
+	ifconfig br-205 10.1.0.1$random netmask 255.255.0.0
+	ping205=$(ping -I br-205 -w10 10.1.0.1 | grep loss | awk '{print $4;exit}')
+	ifconfig br-205 0.0.0.0
 
 # checking if the wireless interface is up or down
-WIFI0=$(! ifconfig |grep "wlan0 " &>/dev/null ; echo $? )
-wlan0_status=$WIFI0
-WIFI1=$(! ifconfig |grep "wlan0-1" &>/dev/null ; echo $? )
-wlan1_status=$WIFI1
-WIFI2=$(! ifconfig |grep "wlan0-2" &>/dev/null ; echo $? )
-wlan2_status=$WIFI2
+	WIFI0=$(! ifconfig |grep "wlan0 " &>/dev/null ; echo $? )
+	wlan0_status=$WIFI0
+	WIFI1=$(! ifconfig |grep "wlan0-1" &>/dev/null ; echo $? )
+	wlan1_status=$WIFI1
+	WIFI2=$(! ifconfig |grep "wlan0-2" &>/dev/null ; echo $? )
+	wlan2_status=$WIFI2
 
 # if it is equal to 1, wifi will be reset
-wifiup=0
+	wifiup=0
 
 # verificando comunicacao na br-205 (em bridge com wlan0)
 # verificando comunicacao na br-203 (em bridge com wlan0-1)
 # verificando comunicacao na br-204 (em bridge com wlan0-2)
 
-for loopcount in "1" "2" "3"; do
+	for loopcount in "1" "2" "3"; do
 	
-	case "$loopcount" in
+		case "$loopcount" in
 	
 		"1")
 		   interface="wlan0"
@@ -66,57 +67,58 @@ for loopcount in "1" "2" "3"; do
 	   	   ;;
    	       	esac
 	
-	if  [ $(! /sbin/ifconfig $interface |/bin/grep UP| /usr/bin/wc -l  ) = "0" ];
-	  then 
+		if  [ $(! /sbin/ifconfig $interface |/bin/grep UP| /usr/bin/wc -l  ) = "0" ];
+	  		then 
 # desligado
-    	  if  [ $pngst -gt 1 ]; 
-		then
+    	  		if  [ $pngst -gt 1 ]; 
+				then
 # pinging, turn on interface, zero status
 # ligar interface, colocar zero no status
-		case "$interface" in
-			"wlan0")
-			wlan0_status=1
-			if [ $WIFI0 -eq 0 ]; then wifiup=1; fi
-			;;
-			"wlan0-1")
-			wlan1_status=1
-			if [ $WIFI1 -eq 0 ]; then wifiup=1; fi
-			;;
-			"wlan0-2")
-			wlan2_status=1
-			if [ $WIFI2 -eq 0 ]; then wifiup=1; fi
-			;;
-		esac
-     		logger SCIFI - Communication with server is ok. Turning $interface on.
-		echo "0"> /tmp/status$interface
-		fi
-	else
+				case "$interface" in
+				"wlan0")
+				wlan0_status=1
+				if [ $WIFI0 -eq 0 ]; then wifiup=1; fi
+				;;
+				"wlan0-1")
+				wlan1_status=1
+				if [ $WIFI1 -eq 0 ]; then wifiup=1; fi
+				;;
+				"wlan0-2")
+				wlan2_status=1
+				if [ $WIFI2 -eq 0 ]; then wifiup=1; fi
+				;;
+				esac
+     				logger SCIFI - Communication with server is ok. Turning $interface on.
+				echo "0"> /tmp/status$interface
+			fi
+		else
 # not pinging
-	case `cat /tmp/status$interface` in
+			case `cat /tmp/status$interface` in
+			
 #  está ligado, está respondendo a ping?
 
-		0)
-		   if [ $pngst = "0" ];
-			then
-			echo "1"> /tmp/status$interface
-			logger SCIFI - The AP can not communicate with server. Warning 1 $interface
+			0)
+		   	if [ $pngst = "0" ];
+				then
+				echo "1"> /tmp/status$interface
+				logger SCIFI - The AP can not communicate with server. Warning 1 $interface
 			fi
-		;;
+			;;
 
-		1)
-		   if [ $pngst = "0" ];
-			then 
+			1)
+		   	if [ $pngst = "0" ];
+				then 
 				echo "2"> /tmp/status$interface
 				logger SCIFI - The AP can not communicate with server. Warning 2 $interface
-			else 
+				else 
 				echo "0"> /tmp/status$interface
 				logger SCIFI - Communication with the server is ok. $interface
 			fi
-		;;
+			;;
 			   	   		   	   		   	       			    	    																												     						  											 																			 				 																												
-		2)
-		   if [ $pngst = "0" ];
-			then
+			2)
+		   	if [ $pngst = "0" ];
+				then
 				logger SCIFI - The AP can not communicate with server. Turning off $interface
 				case $interface in
 					wlan0) wlan0_status=0; ifconfig wlan0 down
@@ -126,42 +128,42 @@ for loopcount in "1" "2" "3"; do
 					"wlan0-2") wlan2_status=0; ifconfig wlan0-2 down
 					;;
 				esac
-			else 
+				else 
 				echo "0"> /tmp/status$interface
 				logger SCIFI - Communication with server is ok. $interface
 			fi
-		;;
-		*) echo "0"> /tmp/status$interface
-		;;
-		esac
+			;;
+			*) echo "0"> /tmp/status$interface
+			;;
+			esac
 		fi
-done
+	done
 
-if [ $wifiup -eq 1 ]
- then
-	logger 'SCIFI - Turning on wlan interfaces...'       
-	wifi
+	if [ $wifiup -eq 1 ]
+ 		then
+		logger 'SCIFI - Turning on wlan interfaces...'       
+		wifi
 
-        if [ $wlan0_status -eq 0 ]
-         then
-	     ifconfig wlan0 down
-        fi
+        	if [ $wlan0_status -eq 0 ]
+         		then
+	     		ifconfig wlan0 down
+        	fi
        
-        if [ $wlan1_status -eq 0 ]
-         then
-             ifconfig wlan0-1 down
-        fi
+        	if [ $wlan1_status -eq 0 ]
+         		then
+             		ifconfig wlan0-1 down
+        	fi
 
-        if [ $wlan2_status -eq 0 ]
-         then
-             ifconfig wlan0-2 down
-        fi
-fi
+        	if [ $wlan2_status -eq 0 ]
+         		then
+             		ifconfig wlan0-2 down
+        	fi
+	fi
 
 # if the AP can not ping on control vlan (i.e, it can not authenticate clients via 802.1x ) it will turn off all wlans interfaces
-else
-        logger 'SCIFI - The AP can not communicate with the server via control vlan. Turning off all wlan interfaces.'
-	wifi down
+	else
+        	logger 'SCIFI - The AP can not communicate with the server via control vlan. Turning off all wlan interfaces.'
+		wifi down
 fi
 
 exit 0
