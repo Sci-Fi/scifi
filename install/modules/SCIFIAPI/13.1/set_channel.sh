a2c01de56a2b885b16f8f9e653c1c1b52acd1e47
helgadb <helgadb@midiacom.uff.br>
2014-12-08 16:42:03 -0200

    Update set_channel.sh
    Adaptation to turn off wireless interfaces that were previously down

--- install/modules/SCIFIAPI/13.1/set_channel.sh
+++ install/modules/SCIFIAPI/13.1/set_channel.sh
@@ -1,4 +1,13 @@
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin;
 logger SCIFI - changing channel to $1
+# getting wireless interfaces and status (up=1 or down=0)
+wInterfaces=$(cat /proc/net/wireless | tail -n +3 | awk -F":" '{print $1}' | sed 's/ //g' | awk '{printf $0; cmd="! ifconfig "$1" | grep UP | wc -l";
+cmd | getline status; print " "status";";close(cmd); }')
 uci set wireless.radio0.channel=$1
-wifi up 
+wifi 
+
+# turning off wireless interfaces that were previously down
+OIFS=$IFS
+IFS=";"
+for i in $wInterfaces; do awk -v i="$i" 'BEGIN{split(i,r," "); print r[1]; print r[2]; if (r[2] == 0 ) {cmd="ifconfig "r[1]" down"; print cmd ; system(cmd) } }';done;
+IFS=$OIFS
