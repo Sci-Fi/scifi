71209b4b4562eee7d776afb22c2450a931ed2a31
helgadb <helgadb@midiacom.uff.br>
2014-12-10 14:23:44 -0200

    corrigindo versao 13.1 para 14

---
+++ install/modules/SCIFIAPI/14/snmpd_watchdog.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# version 20141202
+# this script tries to restart snmpd 10 times, with a 10 second interval. It should run from cron once a day
+#
+# Luiz Magalhaes
+# schara (at) telecom.uff.br
+# set -xv
+
+/etc/init.d/snmpd restart
+sleep 10
+i=0
+while [ $i -lt 10 ];
+        do
+        while [ `ps | grep -c snmpd` -lt 2 ];
+                do
+                /etc/init.d/snmpd restart
+                sleep 10
+                if [ `ps | grep -c snmpd` -gt 1 ];
+                        then exit
+                fi
+                break
+                done
+        let i=i+1
+        done
---
+++ install/modules/SCIFIAPI/14/sta.sh
@@ -0,0 +1,14 @@
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin;
+logger 'SCIFI - collecting data about associated stations'
+rm /tmp/sta.txt
+#remover sta linha quando nao houver necessidade da primeira ser em branco
+echo " " >> /tmp/sta.txt
+iw wlan0 station dump > /tmp/sta_tmp.txt
+cat /tmp/sta_tmp.txt | while read LINE ; do
+	TEST=$(echo $LINE | awk '{ print $1 }')
+	if [ $TEST = "Station" ]
+	then
+	TEST=$(echo $LINE | awk '{ print $2 }')
+	 echo $TEST >> /tmp/sta.txt
+	fi
+		done
